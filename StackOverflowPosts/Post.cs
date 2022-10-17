84aff42debf4167bc3791e38a72e331aba141d25
Kyrylo Salomatin <kirill.salomatin@itperspectives.ro>
2022-10-17 16:41:54 +0300

    Add project files.

---
+++ StackOverflowPosts/Post.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflowPosts
+{
+    public class Post
+    {
+        public string _title { get; set; }
+        public string _content { get; set; }
+        public DateTime _publishTime { get; set; }
+
+        int upVotes = 0;
+        int downVotes = 0;
+
+        public Post() {
+            _title = "Deafault";
+            _content = "Default";
+            _publishTime = DateTime.Now;
+        }
+        public Post(string title)
+            : this()
+        {
+            _title = title;
+            
+
+        }
+        public Post(string title, string content)
+            : this()
+        {
+            _title = title;
+            _content = content;
+            
+        }
+        public Post(string title , string content , DateTime dt)
+            :this()
+        {
+            _title = title;
+            _content = content;
+            _publishTime = dt;
+        }
+
+
+        public void Print()
+        {
+            Console.WriteLine();
+            Console.Write("Title : ");
+            Console.Write(_title + "\n");
+            Console.Write("Content : ");
+            Console.Write(_content + "\n");
+            Console.Write("Date : ");
+            Console.Write(_publishTime.ToString() + "\n");
+            Console.Write("Up votes : ");
+            Console.Write(upVotes + "\n");
+            Console.Write("Down votes : ");
+            Console.Write(downVotes + "\n");
+            Console.WriteLine();
+        }
+
+        public void Upvote()
+        {
+            upVotes++;
+        }
+        public void Downvote()
+        {
+            downVotes++;
+        }
+
+
+    }
+}
---
+++ StackOverflowPosts/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflowPosts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Post>posts = new List<Post>();
+            while (true)
+            {
+                Console.WriteLine("Write number of operation :");
+                Console.WriteLine("1. Add Post");
+                Console.WriteLine("2. Delete Last Post");
+                Console.WriteLine("3. Show post number :");
+                Console.WriteLine("4. Show all posts :");
+                Console.WriteLine("5. Upvote post number :");
+                Console.WriteLine("6. Downvote post number :");
+                int choser = Convert.ToInt32(Console.ReadLine());
+                switch (choser)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Write title : ");
+                            string title = Console.ReadLine();
+                            Console.WriteLine("Write content : ");
+                            string content = Console.ReadLine();
+                            Console.WriteLine("Write Date : ");
+                            string dateStr = Console.ReadLine();
+                            DateTime datePublish = Convert.ToDateTime(dateStr);
+                            posts.Add(new Post(title, content, datePublish));
+                        }
+                        break;
+                    case 2:
+                        {
+                            posts.RemoveAt(posts.Count - 1);
+                        }
+                        break;
+                    case 3:
+                        {
+                            Console.WriteLine("Write position of the post :");
+                            int pos = Convert.ToInt32(Console.ReadLine());
+                            posts[pos].Print();
+                        }
+                        break;
+                    case 4:
+                        {
+                            Console.WriteLine();
+                            for(int i = 0; i < posts.Count; i++)
+                            {
+                                Console.WriteLine($"Post number {i} :");
+                                posts[i].Print();
+                            }
+                        }
+                        break;
+                    case 5:
+                        {
+                            Console.WriteLine("Choose post to upvote ");
+                            int upnum = Convert.ToInt32(Console.ReadLine());
+                            posts[upnum].Upvote();
+                        }
+                        break;
+                    case 6:
+                        {
+                            Console.WriteLine("Choose post to downvote ");
+                            int downnum = Convert.ToInt32(Console.ReadLine());
+                            posts[downnum].Downvote();
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
